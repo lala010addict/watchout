c5983b03fae5fe9a14d392174dc34ffe53735eec
JieYing Peng <jipeng@syr.edu>
2015-10-17 09:46:54 -0700

    player is now draggable omg

--- client/watchout.js
+++ client/watchout.js
@@ -24,34 +24,45 @@ var board = d3.select('body').append('svg')
 var enemies = board.selectAll('.enemies')
                     .data(d3.range(30))
                     .enter()
-                    .append('svg:circle')
-                    .attr('cx', function(){
-                    return(Math.random() * gameOptions.width)
+                    .append('circle')
+                    .attr('cx', function() {
+                      return (Math.random() * gameOptions.width)
                     })
-                    .attr('cy', function(){
-                    return(Math.random() * gameOptions.width)
+                    .attr('cy', function() {
+                      return (Math.random() * gameOptions.width)
                     })
-                    .attr('r',10)
+                    .attr('r', 10)
                     .attr('fill', 'red');
 
 // move fn
 var moveEnemies = function() {
   enemies.transition()
-       .duration(500)
-       .attr('cx', function(){
-         return(Math.random() * gameOptions.width)
-       })
-       .attr('cy', function(){
-         return(Math.random() * gameOptions.width)
-       })
+         .duration(1500)
+         .attr('cx', function(){
+           return (Math.random() * gameOptions.width)
+         })
+         .attr('cy', function(){
+           return(Math.random() * gameOptions.width)
+         })
 };
 
 setInterval(moveEnemies, 2000);
 
 
 // PLAYER STUFF
-var player = board.append("circle")
-  .attr('cx', 150)
-  .attr('cy', 100)
-  .attr('r', 10)
-  .attr('fill', 'black');
+var player = board.append("image")
+                  .attr("x", 150)
+                  .attr("y", 100)
+                  .attr("height", "30px")
+                  .attr("width", "30px")
+                  .attr("xlink:href", "asteroid.png");
+
+var mover = function() {
+  d3.select('image')
+    .attr("x", d3.event.x - parseInt(d3.select('image').attr("width")) / 2)
+    .attr("y", d3.event.y - parseInt(d3.select('image').attr("height")) / 2);
+};
+
+var drag = d3.behavior.drag().on("drag", mover);
+
+d3.select("image").call(drag);
