2b8589ff5d420520b7937e604850afdd3541d779
JieYing Peng <jipeng@syr.edu>
2015-10-17 09:27:13 -0700

    made a player

--- client/watchout.js
+++ client/watchout.js
@@ -1,3 +1,4 @@
+// GENERAL STUFF
 // define gameOptions
 var gameOptions = {
   height: 450,
@@ -16,14 +17,10 @@ var board = d3.select('body').append('svg')
                              .attr('height', gameOptions.height)
                              .attr('width', gameOptions.width)
                              .style('background-color', 'blue')
-// create enemies
-var mouse = board.select('svg')
-  .append("circle")
-  .attr('cx', 150)
-  .attr('cy', 100)
-  .attr('r', 10)
-  .attr('fill', 'black');
 
+
+// ENEMY STUFF
+// create enemies
 var enemies = board.selectAll('.enemies')
                     .data(d3.range(30))
                     .enter()
@@ -36,6 +33,7 @@ var enemies = board.selectAll('.enemies')
                     })
                     .attr('r',10)
                     .attr('fill', 'red');
+
 // move fn
 var moveEnemies = function() {
   enemies.transition()
@@ -50,42 +48,10 @@ var moveEnemies = function() {
 
 setInterval(moveEnemies, 2000);
 
-//create
 
-// var mouse = board.selectAll('.mouse')
-// .append("svg:mouse")
-//  .attr('x', 150)
-//   .attr('y', 100)
-//  .attr('r',10)
-// .attr('fill', 'pink');
-
-// var getNewEnemyData = function() {
-//   // Refactor to use _.range:
-//   [0,1,2,3,4,5].map(function(i) {
-//     return {
-//       id: i,
-//       x: Math.random() * gameOptions.width,
-//       y: Math.random() * gameOptions.height
-//     };
-//   });
-// };
-// render
-
-
-// move enemies
-
-// create player
-  // make draggable
-
-// detect enemy touch
-
-// track score
-// _.range(0, gameOptions.nEnemies) => [0, 1, 2, 3, 4, 5...29]
-
-
-//   _.range(0,gameOptions.nEnemies).map (i) ->
-//     {
-//       id: i
-//       x: Math.random()*100
-//       y: Math.random()*100
-//     }
+// PLAYER STUFF
+var player = board.append("circle")
+  .attr('cx', 150)
+  .attr('cy', 100)
+  .attr('r', 10)
+  .attr('fill', 'black');
